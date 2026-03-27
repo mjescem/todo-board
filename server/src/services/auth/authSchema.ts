0bd19b5f9f8bc7e803d540723e70c8681978faff
jescem <markjescem.stacktrek@gmail.com>
2026-03-28 00:32:14 +0800

    feat(auth): implement sign up

--- server/src/services/auth/authSchema.ts
+++ server/src/services/auth/authSchema.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const SignUpSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
+  password: z.string().min(5, "Password must be at least 5 characters"),
 });
 
 export const LoginSchema = z.object({
