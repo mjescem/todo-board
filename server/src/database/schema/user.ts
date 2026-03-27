183e25b035959f0fdd7e07fba7c486bacee969a0
jescem <markjescem.stacktrek@gmail.com>
2026-03-27 18:33:25 +0800

    feat(server): implement sign up api

--- server/src/database/schema/user.ts
+++ server/src/database/schema/user.ts
@@ -1,9 +1,8 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar({ length: 255 }).notNull(),
 });
