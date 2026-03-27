ef46cdf9b96188aa30ed8ea68c995c1677794849
jescem <markjescem.stacktrek@gmail.com>
2026-03-27 18:46:14 +0800

    feat(server): implement login api

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import * as z from "zod";
-import { signUpUser } from "../services/auth/auth.service.js";
-import { SignUpSchema } from "../services/auth/authSchema.js";
+import { loginUser, signUpUser } from "../services/auth/auth.service.js";
+import { LoginSchema, SignUpSchema } from "../services/auth/authSchema.js";
 
 export async function signUp(req: Request, res: Response) {
   try {
@@ -22,4 +22,25 @@ export async function signUp(req: Request, res: Response) {
     }
     res.status(500).json({ error: "Internal server error" });
   }
+}
+
+export async function login(req: Request, res: Response) {
+    try {
+         const validatedData = LoginSchema.parse(req.body);
+         const result = await loginUser(validatedData);
+         res.status(200).json(result);
+    } catch (error: unknown) {
+      console.info(error);
+      if (error instanceof z.ZodError) {
+        res
+          .status(400)
+          .json({ error: "Validation failed", details: error });
+        return;
+      }
+      if (error instanceof Error && error.message === "Invalid credentials") {
+        res.status(401).json({ error: error.message });
+        return;
+      }
+      res.status(500).json({ error: "Internal server error" });
+    }
 }
